b5260fab54b71d4b774e5ae171e5150f6707ac45
Ian <iankruze60@gmail.com>
2025-04-25 06:25:07 +0300

    edits test.utils and utils.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,24 +3,14 @@ import { isPalindrome } from "../utils";
 
 describe("isPalindrome", () => {
   it("should return true for a simple palindrome", () => {
-    // Arrange
     const word = "racecar";
-
-    // Act
     const result = isPalindrome(word);
-
-    // Assert
     expect(result).toBe(true);
   });
 
   it("should return false for a non-palindrome", () => {
-    // Arrange
     const word = "car";
-
-    // Act
     const result = isPalindrome(word);
-
-    // Assert
     expect(result).toBe(false);
   });
 });
